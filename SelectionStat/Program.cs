7d618c3a8311cc48e2e01981096191a3495709c3
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-29 16:45:08 -0500

    initial commit

---
+++ SelectionStat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(30);
+            Console.WriteLine("Try to guess my favorite number");
+            var userlnput = int.Parse(Console.ReadLine());
+            if (userlnput < 30)
+            {
+                Console.WriteLine($"Too low");
+            }
+
+            else if (userlnput > 30)
+            {
+                Console.WriteLine($"Too high");
+            }
+            else 
+            {
+                Console.WriteLine($"Nevermind");
+            }
+
+
+
+
+           
+        }
+    }
+}
